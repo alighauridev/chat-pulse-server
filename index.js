672afd2ea7f3c501db801e1f1974a4b1e6e1ff9d
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-09-28 11:08:27 +0500

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import * as Server from 'socket.io';
 
 const app = express();
 const corsConfig = {
-  origin: 'http://localhost:3000',
+  origin: 'https://chat-pulse-client.vercel.app/',
   credentials: true,
 };
 
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server.Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-pulse-client.vercel.app/',
   },
 });
 io.on('connection', (socket) => {
